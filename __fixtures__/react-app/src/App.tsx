19fe7c59398d562838d7dab8442b574555d4357b
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2021-09-16 21:56:02 +0700

    chore: custom sample react-app

--- __fixtures__/react-app/src/App.tsx
+++ __fixtures__/react-app/src/App.tsx
@@ -1,23 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { PlusOutlineIcon } from 'react-icons-boilerplate';
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
+        <PlusOutlineIcon className="App-logo" color="#125D98" width={200} height={200} />
         <p>
           <a
             className="App-link"
@@ -27,7 +17,6 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
